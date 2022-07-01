76630f23e8a2985f905b1b39cad55122db755deb
yyx0111 <749857410@qq.com>
2022-07-01 15:57:00 +0800

    节目编辑

--- src/main/java/com/example/demo/controller/ProgramController.java
+++ src/main/java/com/example/demo/controller/ProgramController.java
@@ -39,101 +39,19 @@ public class ProgramController {
     @Resource
     ProgramMapper programMapper;
     //新增
+    //新增
     @PostMapping
-    public Result save(String program_name,String resolving_power,String program_time,String program_size,Integer program_state,String program_author,MultipartFile file, HttpServletRequest request)throws FileNotFoundException {
-        Program program=new Program();
+    public Result save(@RequestBody Program program){
         program.setProgramUpdate(LocalDateTime.now());
-        program.setProgramAuthor(program_author);
-        program.setProgramName(program_name);
-        program.setProgramState(program_state);
-        program.setProgramAuthor(program_author);
-        program.setProgramTime(program_time);
-        program.setProgramSize(program_size);
-        program.setResolvingPower(resolving_power);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("/yyyy-MM-DD/");
-        String filename = file.getOriginalFilename();//取得文件名称
-        if (!filename.endsWith(".png") && !filename.endsWith(".jpg")) {
-            return Result.fail("文件类型不对");
-        }
-
-        String format = dateFormat.format(new Date(System.currentTimeMillis()));
-        //realPath为文件保存的位置
-        String realPath = ResourceUtils.getURL("classpath:").getPath() + "static/img" + format;
-        realPath = realPath.substring(1);//去掉第一个/符号
-        File folder = new File(realPath);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-        String newName;
-        if (filename.endsWith(".png"))
-            newName = UUID.randomUUID().toString() + ".png";
-        else
-            newName = UUID.randomUUID().toString() + ".jpg";
-        try {
-            //新建文件
-            file.transferTo(new File(folder, newName));
-            //String url = realPath + newName;
-        } catch (IOException e) {
-            return Result.fail(e.getMessage());
-        }
-
-        //返回可供外界访问的url
-        String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/img" + format + newName;
-        program.setProgramImg(url);
         programMapper.insert(program);
+
         return Result.succ(200,"新增成功",program);
     }
     //更新
     @PutMapping
-    public Result update(Integer program_id,String program_name,String resolving_power,String program_time,String program_size,Integer program_state,String program_author,MultipartFile file, HttpServletRequest request)throws FileNotFoundException {
-        Program program=new Program();
-        program.setProgramId(program_id);
-        program.setProgramUpdate(LocalDateTime.now());
-        program.setProgramAuthor(program_author);
-        program.setProgramName(program_name);
-        program.setProgramState(program_state);
-        program.setProgramAuthor(program_author);
-        program.setProgramTime(program_time);
-        program.setProgramSize(program_size);
-        program.setResolvingPower(resolving_power);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("/yyyy-MM-DD/");
-        String filename = file.getOriginalFilename();//取得文件名称
-        if (!filename.endsWith(".png") && !filename.endsWith(".jpg")) {
-            return Result.fail("文件类型不对");
-        }
-
-        String format = dateFormat.format(new Date(System.currentTimeMillis()));
-        //realPath为文件保存的位置
-        String realPath = ResourceUtils.getURL("classpath:").getPath() + "static/img" + format;
-        realPath = realPath.substring(1);//去掉第一个/符号
-        File folder = new File(realPath);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-        String newName;
-        if (filename.endsWith(".png"))
-            newName = UUID.randomUUID().toString() + ".png";
-        else
-            newName = UUID.randomUUID().toString() + ".jpg";
-        try {
-            //新建文件
-            file.transferTo(new File(folder, newName));
-            //String url = realPath + newName;
-        } catch (IOException e) {
-            return Result.fail(e.getMessage());
-        }
-
-        //返回可供外界访问的url
-        String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/img" + format + newName;
-        program.setProgramImg(url);
+    public Result update(@RequestBody Program program){
         programMapper.updateById(program);
         return Result.succ(200,"更新成功",program);
-    }
-    //重命名
-    @PutMapping("/rename")
-    public Result reName(@RequestBody  Program program){
-        programMapper.reName(program.getProgramName(),program.getProgramId());
-        return Result.succ(200,"重命名成功",program);
     }
     //删除
     @DeleteMapping("/{id}")
