b48f8f437200559b3ccd9986276a3b750a82485f
yyx0111 <749857410@qq.com>
2022-07-05 09:20:01 +0800

    pub

---
+++ src/main/java/com/example/demo/service/PubProgramService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.PubProgram;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author anonymous
+ * @since 2022-07-05
+ */
+public interface PubProgramService extends IService<PubProgram> {
+
+}
---
+++ src/main/java/com/example/demo/service/impl/PubProgramServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.PubProgram;
+import com.example.demo.mapper.PubProgramMapper;
+import com.example.demo.service.PubProgramService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author anonymous
+ * @since 2022-07-05
+ */
+@Service
+public class PubProgramServiceImpl extends ServiceImpl<PubProgramMapper, PubProgram> implements PubProgramService {
+
+}
